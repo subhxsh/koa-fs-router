43b07ffae1f8ce187a95814adcacd10d8f5018d8
retr0punk <me@retr0punk.xyz>
2026-01-04 16:17:31 +0530

    feat: fully implement route/radix tree

--- src/route-tree.ts
+++ src/route-tree.ts
@@ -9,35 +9,33 @@ type StaticNode = {
 
 type DynamicNode = {
   param: string;
-  optional: boolean;
+  optional?: never;
   filePath?: string;
   children?: Children;
 };
 
-type WildcardNode = {
+type CatchAllNode = {
   param: string;
   optional: boolean;
   filePath: string;
   children?: never;
 };
 
-type AnyNode = StaticNode | DynamicNode | WildcardNode;
+type AnyNode = StaticNode | DynamicNode | CatchAllNode;
 
 type Children = {
   static?: Record<string, StaticNode>;
   dynamic?: DynamicNode;
-  wildcard?: WildcardNode;
+  catchAll?: CatchAllNode;
 };
 
 export const createRouteTree = () => {
   const rootNode: StaticNode = {};
 
-  const fmt = (
-    node: { param: string; optional: boolean },
-    wildcard = false,
-  ) => {
+  const fmtDynamic = (node: { param: string }) => `[${node.param}]`;
+  const fmtCatchAll = (node: { param: string; optional: boolean }) => {
     const count = node.optional ? 2 : 1;
-    return `${"[".repeat(count)}${wildcard ? `...${node.param}` : node.param}${"]".repeat(count)}`;
+    return `${"[".repeat(count)}...${node.param}${"]".repeat(count)}`;
   };
 
   const insert = (relativePath: string) => {
@@ -61,54 +59,58 @@ export const createRouteTree = () => {
         const optional = param.startsWith("[") && param.endsWith("]");
         if (optional) param = param.slice(1, -1);
 
-        const wildcard = param.startsWith("...");
-        if (wildcard) {
+        const catchAll = param.startsWith("...");
+        if (catchAll) {
           param = param.slice(3);
 
           if (!isLast)
             throw new Error(
-              `${segment} in '${relativePath}' must be the last part of the URL`,
+              `Catch-all segment ${segment} in '${relativePath}' must be the last part of the URL`,
             );
 
           if (!currentNode.children) currentNode.children = {};
           if (currentNode.children.dynamic)
             throw new Error(
-              `${segment} in '${relativePath}' conflicts with ${fmt(currentNode.children.dynamic)}`,
+              `Catch-all segment ${segment} in '${relativePath}' cannot exist with a dynamic segment ${fmtDynamic(currentNode.children.dynamic)} on the same level`,
             );
 
-          if (currentNode.children.wildcard)
+          if (currentNode.children.catchAll)
             throw new Error(
-              `${segment} in '${relativePath}' conflicts with ${fmt(currentNode.children.wildcard, true)}`,
+              `Catch-all segment ${segment} in '${relativePath}' cannot exist with another catch-all segment ${fmtCatchAll(currentNode.children.catchAll)} on the same level`,
             );
 
-          currentNode = currentNode.children.wildcard = {
+          currentNode = currentNode.children.catchAll = {
             param,
             optional,
             filePath: relativePath,
           };
         } else {
-          if (!currentNode.children) currentNode.children = {};
-          if (currentNode.children.wildcard)
+          if (optional)
             throw new Error(
-              `${segment} in '${relativePath}' conflicts with ${fmt(currentNode.children.wildcard, true)}`,
+              `Dynamic segment ${segment} in '${relativePath}' cannot be optional`,
+            );
+
+          if (!currentNode.children) currentNode.children = {};
+          if (currentNode.children.catchAll)
+            throw new Error(
+              `Dynamic segment ${segment} in '${relativePath}' cannot exist with a catch-all segment ${fmtCatchAll(currentNode.children.catchAll)} on the same level`,
             );
 
           if (
             currentNode.children.dynamic &&
-            (currentNode.children.dynamic.param !== param ||
-              currentNode.children.dynamic.optional !== optional)
+            currentNode.children.dynamic.param !== param
           )
             throw new Error(
-              `${segment} in '${relativePath}' has different specificity than ${fmt(currentNode.children.dynamic)}`,
+              `Dynamic segment ${segment} in '${relativePath}' uses a different parameter name than ${fmtDynamic(currentNode.children.dynamic)}`,
             );
 
           if (isLast && currentNode.children.dynamic?.filePath)
             throw new Error(
-              `${segment} in '${relativePath}' conflicts with ${fmt(currentNode.children.dynamic)}`,
+              `Dynamic segment ${segment} in '${relativePath}' cannot exist with another dynamic segment ${fmtDynamic(currentNode.children.dynamic)} on the same level`,
             );
 
           if (!currentNode.children.dynamic)
-            currentNode.children.dynamic = { param, optional };
+            currentNode.children.dynamic = { param };
 
           if (isLast) currentNode.children.dynamic.filePath = relativePath;
           currentNode = currentNode.children.dynamic;
@@ -117,7 +119,7 @@ export const createRouteTree = () => {
         if (isLast && segment === "index") {
           if (currentNode.filePath)
             throw new Error(
-              `'${segment}' in '${relativePath}' conflicts with '${currentNode.filePath}'`,
+              `Static segment '${segment}' in '${relativePath}' conflicts with '${currentNode.filePath}'`,
             );
           currentNode.filePath = relativePath;
           return;
@@ -128,7 +130,7 @@ export const createRouteTree = () => {
 
         if (isLast && currentNode.children.static[segment]?.filePath)
           throw new Error(
-            `'${segment}' in '${relativePath}' conflicts with '${currentNode.children.static[segment].filePath}'`,
+            `Static segment '${segment}' in '${relativePath}' conflicts with ${currentNode.children.static[segment].filePath}`,
           );
 
         if (!currentNode.children.static[segment])
@@ -141,5 +143,53 @@ export const createRouteTree = () => {
     }
   };
 
-  return { insert };
+  const match = (path: string) => {
+    let currentNode: AnyNode = rootNode;
+
+    const segments = path.split("/").filter(Boolean);
+    const params: Record<string, string | string[]> = {};
+
+    for (let i = 0; i < segments.length; i++) {
+      const segment = segments[i]!;
+
+      if (!currentNode.children) return;
+
+      if (currentNode.children.static?.[segment]) {
+        currentNode = currentNode.children.static[segment];
+        continue;
+      }
+
+      if (currentNode.children.dynamic) {
+        params[currentNode.children.dynamic.param] = segment;
+        currentNode = currentNode.children.dynamic;
+        continue;
+      }
+
+      if (currentNode.children.catchAll) {
+        params[currentNode.children.catchAll.param] = segments.slice(i);
+        currentNode = currentNode.children.catchAll;
+        break;
+      }
+
+      return;
+    }
+
+    if (!currentNode.filePath && currentNode.children?.catchAll?.optional) {
+      params[currentNode.children.catchAll.param] = [];
+      currentNode = currentNode.children.catchAll;
+    }
+
+    if (currentNode.filePath)
+      return {
+        params,
+        filePath: currentNode.filePath,
+      };
+
+    return;
+  };
+
+  return {
+    insert,
+    match,
+  };
 };
